0c3b9ecd75dc1daa0f981eda9b269c7ba8cc88a5
van <not>
2023-05-16 22:26:16 +0800

    fix

--- src/Yunxin.php
+++ src/Yunxin.php
@@ -8,6 +8,7 @@ namespace sndwow\yunxin;
 
 use Yii;
 use yii\base\Component;
+use yii\helpers\ArrayHelper;
 
 /**
  * @property User $user
@@ -15,29 +16,44 @@ use yii\base\Component;
  */
 class Yunxin extends Component
 {
-    // 网易云信分配的账号
-    public ?string $appKey = null;
     
-    // 网易云信分配的密钥
-    public ?string $appSecret = null;
-    
-    // 请求超时时间
-    public int $timeout = 5;
+    public ?string $appKey = null; // 网易云信分配的账号
+    public ?string $appSecret = null; // 网易云信分配的密钥
+    public int $timeout = 5; // 请求超时时间
     
     public array $queue = [];
     
+    private ?object $_queue = null;
     private ?User $_user = null;
     private ?Chatroom $_chatroom = null;
+    
+    private function queue()
+    {
+        if (!$this->queue) {
+            return null;
+        }
+        
+        if (!$this->_queue) {
+            $this->_queue = Yii::createObject(ArrayHelper::merge($this->queue, [
+                'class' => yii\queue\amqp_interop\Queue::class,
+                'as log' => yii\queue\LogBehavior::class,
+                'strictJobType' => false,
+                'serializer' => \yii\queue\serializers\JsonSerializer::class,
+                'qosPrefetchCount' => 500,
+            ]));
+        }
+        
+        return $this->_queue;
+    }
     
     public function getUser()
     {
         if (!$this->_user) {
-            $q = $this->queue;
             $this->_user = new User();
             $this->_user->appKey = $this->appKey;
             $this->_user->appSecret = $this->appSecret;
             $this->_user->timeout = $this->timeout;
-            $this->_user->queue = Yii::$app->$q;
+            $this->_user->queue = $this->queue();
         }
         return $this->_user;
     }
@@ -45,12 +61,11 @@ class Yunxin extends Component
     public function getChatroom()
     {
         if (!$this->_chatroom) {
-            $q = $this->queue;
             $this->_chatroom = new Chatroom();
             $this->_chatroom->appKey = $this->appKey;
             $this->_chatroom->appSecret = $this->appSecret;
             $this->_chatroom->timeout = $this->timeout;
-            $this->_chatroom->queue = Yii::$app->$q;
+            $this->_chatroom->queue = $this->queue();
         }
         return $this->_user;
     }
